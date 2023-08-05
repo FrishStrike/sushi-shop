ff77793ef3db4eb8962c3b39556c744769959364
unknown <FrishStrike@mail.ru>
2023-08-05 22:15:54 +0300

    Finish 3

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,15 +2,12 @@ import { transporter } from "@/configs/nodemailer";
 
 export const sendEmail = async (userEmail, text) => {
   try {
-    await transporter.sendMail(
-      {
-        from: process.env.EMAIL,
-        to: userEmail.toLowerCase(),
-        text: text,
-        subject: "code",
-      },
-      (err) => console.log(err)
-    );
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: userEmail.toLowerCase(),
+      text: text,
+      subject: "code",
+    });
   } catch (error) {
     throw new Error("Email error");
   }
